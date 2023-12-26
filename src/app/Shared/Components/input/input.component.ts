286c88e6b068e49fe11675e4b74b7b3cdd33a1f7
rusagon04 <rsgg0411@gmail.com>
2023-12-26 12:05:59 -0400

    Fue creado de forma generica el input

---
+++ src/app/Shared/Components/input/input.component.ts
@@ -0,0 +1,80 @@
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
+})
+export class InputComponent implements OnInit, ControlValueAccessor{
+  
+  _ClassInput: string = "form-control shadow bg-body rounded";
+  _PlaceHolder: string = "";
+  _Label: string = "";
+  _CurrentValue: string | number = "";
+  
+  onChange = (_: any) => { };
+  onTouch = () => { };
+
+  @Input("ClassInput")
+  set ClassInput(classInput: string){
+    this._ClassInput = classInput ? classInput : "form-control shadow bg-body rounded";
+  }
+
+  @Input("PlaceHolder")
+  set PlaceHolder(placeHolder: string){
+    this._PlaceHolder = placeHolder ? placeHolder : "";
+  }
+
+  @Input("Label")
+  set Label(label: string){
+    this._Label = label ? label : "";
+  }
+  
+  get ClassInput(){
+    return this._ClassInput;
+  }
+
+  get PlaceHolder(){
+    return this._PlaceHolder;
+  }
+
+  get Label(){
+    return this._Label;
+  }
+
+  get CurrentValue(){
+    return this._CurrentValue;
+  }
+
+  constructor() {}
+
+
+  ngOnInit(): void { }
+
+  ChangeValue(e: any) {
+    this._CurrentValue = e.target.value;
+    this.onTouch();
+    this.onChange(this._CurrentValue);
+  }
+
+  writeValue(value: string | number): void {
+    this._CurrentValue = value ? value : "";
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+}
